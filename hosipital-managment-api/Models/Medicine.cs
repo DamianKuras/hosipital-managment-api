5a8baad8b66a1df7ab761e2fb0229f80ac043629
DamianKuras <damian_kuras@outlook.com>
2022-07-24 23:18:44 +0200

    feat: add Medicine model

---
+++ hosipital-managment-api/Models/Medicine.cs
@@ -0,0 +1,11 @@
+﻿namespace hosipital_managment_api.Models
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+
+    }
+}

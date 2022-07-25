62fa14dda8efdc0f4dc41fbad64e4aea76cd5702
DamianKuras <damian_kuras@outlook.com>
2022-07-25 22:48:34 +0200

    feat: add database context

---
+++ hosipital-managment-api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace hosipital_managment_api.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Medicne> Medicines;
+    }
+}

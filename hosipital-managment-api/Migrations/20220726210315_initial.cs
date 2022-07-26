8169d5021ab7fce66eddce7756e81dbe7a93e33e
DamianKuras <damian_kuras@outlook.com>
2022-07-26 23:36:51 +0200

    feat: add migration
    added database password to conention string comming from user secret DbPassword in Program.cs

---
+++ hosipital-managment-api/Migrations/20220726210315_initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace hosipital_managment_api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicines", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicines");
+        }
+    }
+}
--- hosipital-managment-api/Program.cs
+++ hosipital-managment-api/Program.cs
@@ -10,9 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var connection = builder.Configuration.GetConnectionString("AppDbConnection") + ";password=" + builder.Configuration["DbPassword"].ToString();
 builder.Services.AddDbContext<AppDbContext>(options => options
-.UseNpgsql(builder.Configuration.GetConnectionString("AppDbContext")));
+.UseNpgsql(connection));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

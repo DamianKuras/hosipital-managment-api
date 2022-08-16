bf6e7bf6022cbe4eec902dcda529b8685d80e0d6
DamianKuras <damian_kuras@outlook.com>
2022-08-16 22:38:42 +0200

    feat: add Prescription Repository

---
+++ hosipital-managment-api/Interface/IPrescriptionRepository.cs
@@ -0,0 +1,16 @@
+﻿using hosipital_managment_api.Models;
+
+namespace hosipital_managment_api.Interface
+{
+    public interface IPrescriptionRepository
+    {
+        public Task<IEnumerable<Prescription>> GetPrescriptionsForDoctor(string DoctorId);
+        public Task<IEnumerable<Prescription>> GetPrescriptionsForPatient(string PatientId);
+        public Task<Prescription> GetPrescription(int id);
+
+        public Task<bool> CreatePrescription(Prescription prescription);
+        public Task<bool> UpdatePrescription(Prescription prescription);
+        public Task<bool> DeletePrescription(Prescription prescription);
+        public Task<bool> PrescriptionExist(int id);
+    }
+}
---
+++ hosipital-managment-api/Repository/PrescriptionRepository.cs
@@ -0,0 +1,59 @@
+﻿using hosipital_managment_api.Data;
+using hosipital_managment_api.Interface;
+using hosipital_managment_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace hosipital_managment_api.Repository
+{
+    public class PrescriptionRepository : IPrescriptionRepository
+    {
+        private readonly AppDbContext _context;
+        public PrescriptionRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> CreatePrescription(Prescription prescription)
+        {
+            _context.Prescriptions.Add(prescription);
+            return await Save();
+        }
+
+        public async Task<bool> DeletePrescription(Prescription prescription)
+        {
+            _context.Remove(prescription);
+            return await Save();
+        }
+
+        public async Task<Prescription> GetPrescription(int id)
+        {
+            return await _context.Prescriptions.Where(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Prescription>> GetPrescriptionsForDoctor(string DoctorId)
+        {
+            return await _context.Prescriptions.Where(p => p.Doctor.Id == DoctorId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Prescription>> GetPrescriptionsForPatient(string PatientId)
+        {
+            return await _context.Prescriptions.Where(p => p.Patient.Id == PatientId).ToListAsync();
+        }
+
+        public async Task<bool> PrescriptionExist(int id)
+        {
+            return await _context.Prescriptions.AnyAsync(p => p.Id == id);
+        }
+
+        public async Task<bool> Save()
+        {
+            var saved = await _context.SaveChangesAsync();
+            return saved > 0;
+        }
+
+        public async Task<bool> UpdatePrescription(Prescription prescription)
+        {
+            _context.Update(prescription);
+            return await Save();
+        }
+    }
+}

447697f5c99c429f765bd6e7d0a0f8305396f6e2
DamianKuras <damian_kuras@outlook.com>
2022-09-11 23:00:36 +0200

    feat: add Appointment and Schedule models

---
+++ hosipital-managment-api/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using hosipital_managment_api.Data;
+
+namespace hosipital_managment_api.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public ApiUser Patient { get; set; }
+        public string PatientId { get; set; }
+        public ApiUser Doctor { get; set; }
+        public string DoctorId { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string Status { get; set; }
+        
+    }
+}
---
+++ hosipital-managment-api/Models/Schedule.cs
@@ -0,0 +1,12 @@
+﻿using hosipital_managment_api.Data;
+
+namespace hosipital_managment_api.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public TimeOnly TimePerPatient { get; set; }
+        public TimeOnly Start { get; set; }
+        public TimeOnly End {get;set;}
+    }
+}
